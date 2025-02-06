dc1ffdd969722f4c98543c29a5f04d122f07df18
Farjad <farjadkhalil7@gmail.com>
2025-02-06 13:25:04 +0500

    Created Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router'
 function Navbar() {
-  return (
-    <div className='mx-auto flex w-full justify-between'>
-      <Link>Link 1</Link>
-      <Link>Link 1</Link>
-      <Link>Link 1</Link>
-      <Link>Link 1</Link>
+   return (
+      <nav className='flex justify-between gap-10'>
+         <div className='flex gap-10'>
+            <button className='text-3xl font-bold'>LOGO</button>
+            <Link   className='transition-all  hover:bg-black duration-300 p-2.5 rounded-lg'>Link 1</Link>
+            <Link   className='transition-all  hover:bg-black duration-300 p-2.5 rounded-lg'>Link 1</Link>
+            <Link   className='transition-all  hover:bg-black duration-300 p-2.5 rounded-lg'>Link 1</Link>
 
-      <Link>searchbar</Link>
-      <Link>Cart</Link>
-      <Link>Dark Mode</Link>
-      <Link>Sign In</Link>
-    </div>
-  )
+         </div>
+
+         <div className='flex gap-2'>
+            <Link   className='material-symbols-outlined transition-all  hover:bg-black duration-300 p-2.5 rounded-lg'>search</Link>
+            <Link   className='material-symbols-outlined transition-all  hover:bg-black duration-300 p-2.5 rounded-lg'>shopping_cart</Link>
+            <Link   className='transition-all  hover:bg-black hover:text-white hover:border-white border w-20 h-10 text-center duration-300 p-1.5 rounded-lg bg-white text-black'>Sign In</Link>
+         </div>
+      </nav>
+   )
 }
 
 export default Navbar
